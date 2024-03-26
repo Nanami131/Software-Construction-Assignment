43e573c246d2e8fda87fcff25e847f68be9b2371
Nanami <2920383858@qq.com>
2024-03-27 00:25:33 +0800

    Sieve of Eratosthenes

---
+++ homework-002/Eratosthenes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool[] isPrime = new bool[101];
+        for (int i = 2; i <= 100; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= 100; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j <= 100; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        Console.WriteLine("2~100 之间的素数为:");
+        for (int i = 2; i <= 100; i++)
+        {
+            if (isPrime[i])
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
